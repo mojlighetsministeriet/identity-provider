a20cf0eefdb037d0ca06fa12296310bae147c53c
Fredrik Söderström <fredrik.soderstrom@cambio.se>
2017-07-07 20:22:53 +0200

    Start implementing the API

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+
+	validator "gopkg.in/go-playground/validator.v9"
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/mojlighetsministeriet/identity-provider/users"
+	uuid "github.com/satori/go.uuid"
 )
 
 // Service will hold the entire HTTP service
@@ -16,27 +20,143 @@ type Service struct {
 	DatabaseConnection *gorm.DB
 }
 
-// Setup database connection and all the routes
-func (service *Service) Setup(databaseType string, databaseConnectionString string) {
+func (service *Service) getUsersListHandler(response http.ResponseWriter, request *http.Request) {
+	var usersList []users.User
+	err := service.DatabaseConnection.Find(&usersList).Error
+	if err != nil {
+		log.Fatal(err)
+		respondWithError(response, 500, "Internal Server Error")
+	} else {
+		respondWithJSON(response, 200, usersList)
+	}
+}
+
+func (service *Service) getUserHandler(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	user := users.User{}
+	result := service.DatabaseConnection.Where("id = ?", vars["id"]).First(&user)
+	if result.Error != nil {
+		if result.RecordNotFound() {
+			respondWithError(response, 404, "Not Found")
+		} else {
+			log.Fatal(result.Error)
+			respondWithError(response, 500, "Internal Server Error")
+		}
+	} else {
+		respondWithJSON(response, 200, user)
+	}
+}
+
+func (service *Service) updateUserHandler(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	userID := vars["id"]
+	user := users.User{}
+	decoder := json.NewDecoder(request.Body)
+	defer request.Body.Close()
+
+	err := decoder.Decode(&user)
+	if err != nil {
+		respondWithError(response, 400, "Bad Request")
+		return
+	}
+
+	user.ID, err = uuid.FromString(userID)
+	if err != nil {
+		respondWithError(response, 400, "Bad Request")
+		return
+	}
+
+	validate := validator.New()
+	err = validate.Struct(user)
+	if err != nil {
+		respondWithJSON(response, 400, err)
+		return
+	}
+
+	err = service.DatabaseConnection.Update(user).Error
+	if err != nil {
+		log.Fatal(err)
+		respondWithError(response, 500, "Internal Server Error")
+	} else {
+		respondWithJSON(response, 200, user)
+	}
+}
+
+func (service *Service) createUserHandler(response http.ResponseWriter, request *http.Request) {
+	user := users.User{}
+	decoder := json.NewDecoder(request.Body)
+	defer request.Body.Close()
+
+	err := decoder.Decode(&user)
+	if err != nil {
+		respondWithError(response, 400, "Bad Request")
+		return
+	}
+
+	validate := validator.New()
+	err = validate.Struct(user)
+	if err != nil {
+		respondWithJSON(response, 400, err)
+		return
+	}
+
+	err = service.DatabaseConnection.Save(user).Error
+	if err != nil {
+		log.Fatal(err)
+		respondWithError(response, 500, "Internal Server Error")
+	} else {
+		respondWithJSON(response, 201, user)
+	}
+}
+
+func (service *Service) deleteUserHandler(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	userID := vars["id"]
+	user := users.User{}
 	var err error
-	service.DatabaseConnection, err = gorm.Open(databaseType, databaseConnectionString)
+	user.ID, err = uuid.FromString(userID)
+	if err != nil {
+		respondWithError(response, 400, "Bad Request")
+		return
+	}
+
+	err = service.DatabaseConnection.Delete(user).Error
+	if err != nil {
+		log.Fatal(err)
+		respondWithError(response, 500, "Internal Server Error")
+	} else {
+		respondWithJSON(response, 200, user)
+	}
+}
+
+// Setup database connection and all the routes
+func (service *Service) Setup(databaseType string, DatabaseConnectionString string, debug bool) {
+	var err error
+	service.DatabaseConnection, err = gorm.Open(databaseType, DatabaseConnectionString)
 	if err != nil {
 		panic("Failed to connect database")
 	}
+
+	if debug == true {
+		service.DatabaseConnection.Debug()
+	}
+
 	service.DatabaseConnection.AutoMigrate(&users.User{})
 
 	service.Router = mux.NewRouter()
-	service.Router.HandleFunc("/", service.apiInfoHandler)
 
-	service.Router.HandleFunc("/token", service.generateTokenHandler).Methods("POST")
-	service.Router.HandleFunc("/token/renew", service.renewTokenHandler).Methods("POST")
-	service.Router.HandleFunc("/token", service.deleteTokenHandler).Methods("DELETE")
+	/*
+		service.Router.HandleFunc("/", service.apiInfoHandler)
 
-	id := "{id:[0-9a-f]{8}\\-[0-9a-f]{4}\\-[0-9a-f]{4}\\-[0-9a-f]{4}\\-[0-9a-f]{12}}"
-	service.Router.HandleFunc("/user", service.listUsersHandler).Methods("GET")
-	service.Router.HandleFunc("/user/"+id, service.updateUserHandler).Methods("PUT")
+		service.Router.HandleFunc("/token", service.generateTokenHandler).Methods("POST")
+		service.Router.HandleFunc("/token/renew", service.renewTokenHandler).Methods("POST")
+		service.Router.HandleFunc("/token", service.deleteTokenHandler).Methods("DELETE")
+	*/
+	service.Router.HandleFunc("/user", service.getUsersListHandler).Methods("GET")
+	service.Router.HandleFunc("/user/{id}", service.getUserHandler).Methods("GET")
+	service.Router.HandleFunc("/user/{id}", service.updateUserHandler).Methods("PUT")
 	service.Router.HandleFunc("/user", service.createUserHandler).Methods("POST")
-	service.Router.HandleFunc("/user/"+id, service.deleteUserHandler).Methods("DELETE")
+	service.Router.HandleFunc("/user/{id}", service.deleteUserHandler).Methods("DELETE")
 
 	//http.Handle("/", service.Router)
 }
@@ -51,7 +171,7 @@ func (service *Service) Listen(address string) {
 
 func main() {
 	service := Service{}
-	service.Setup("mysql", "root:hej256@/identity-provider?charset=utf8&parseTime=True&loc=Local")
+	service.Setup("mysql", "root:hej256@/identity-provider?charset=utf8&parseTime=True&loc=Local", false)
 	service.Listen(":8080")
 }
 
--- users/user.go
+++ users/user.go
@@ -1,15 +1,30 @@
 package users
 
 import (
-	"github.com/jinzhu/gorm"
+	"strings"
+	"time"
+
 	uuid "github.com/satori/go.uuid"
 )
 
 // User represents a user
 type User struct {
-	gorm.Model
-	ID       uuid.UUID `json:"id" gorm:"primary_key"`
-	Email    string    `json:"email"`
-	Password string    `json:"-"`
-	Roles    []string  `json:"roles"`
+	ID          uuid.UUID  `sql:"id;not null;type:varchar(36);primary_key" validate:"uuid4"`
+	CreatedAt   time.Time  `sql:"not null"`
+	UpdatedAt   time.Time  `validate:"gtefield=CreatedAt"`
+	DeletedAt   *time.Time `sql:"index" validate:"gtefield=CreatedAt"`
+	Email       string     `sql:"not null" json:"email" validate:"email"`
+	Password    string     `json:"-"`
+	Roles       []string   `json:"roles" sql:"-" validate:"dive,eq=user|eq=admin|eq=super-admin"`
+	RolesString string     `json:"-" sql:"roles;not null"`
+}
+
+// AfterFind splits the Roles field after loaded
+func (user *User) AfterFind() {
+	user.Roles = strings.Split(user.RolesString, ";")
+}
+
+// BeforeSave populates the rolesString field before saving to database
+func (user *User) BeforeSave() {
+	user.RolesString = strings.Join(user.Roles, ";")
 }
