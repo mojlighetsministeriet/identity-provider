025e97cedd9532d58ad595c98c5ba80fb4771736
Fredrik Söderström <fredrik.soderstrom@cambio.se>
2017-09-10 01:14:56 +0200

    Adds tests

--- token/middleware_test.go
+++ token/middleware_test.go
@@ -3,6 +3,7 @@ package token_test
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -16,7 +17,7 @@ import (
 func TestGetTokenFromContext(test *testing.T) {
 	request := httptest.NewRequest(echo.GET, "/", nil)
 	request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	privateKey, err := rsa.GenerateKey(rand.Reader, 512)
 	assert.NoError(test, err)
 
 	account := entity.Account{
@@ -34,4 +35,76 @@ func TestGetTokenFromContext(test *testing.T) {
 
 	extractedToken := token.GetTokenFromContext(context)
 	assert.Equal(test, accessToken, extractedToken)
+}
+
+func TestJWTRequiredRoleMiddlewareWhenMissingAuthorizationToken(test *testing.T) {
+	server := echo.New()
+	request := httptest.NewRequest(echo.GET, "/", nil)
+	recorder := httptest.NewRecorder()
+	context := server.NewContext(request, recorder)
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 512)
+	assert.NoError(test, err)
+
+	middleware := token.JWTRequiredRoleMiddleware(&privateKey.PublicKey, "administrator")
+	handler := middleware(echo.HandlerFunc(func(context echo.Context) error {
+		return context.NoContent(http.StatusOK)
+	}))
+	handler(context)
+
+	assert.Equal(test, recorder.Result().StatusCode, http.StatusUnauthorized)
+}
+
+func TestJWTRequiredRoleMiddlewareWhenMissingRequiredAdministratorRole(test *testing.T) {
+	server := echo.New()
+	request := httptest.NewRequest(echo.GET, "/", nil)
+	recorder := httptest.NewRecorder()
+	context := server.NewContext(request, recorder)
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 512)
+	assert.NoError(test, err)
+
+	account := entity.Account{
+		ID:    uuid.NewV4(),
+		Email: "tech+testing@mojlighetsministerietest.se",
+		Roles: []string{"user"},
+	}
+	accessToken, err := token.Generate(privateKey, account)
+	assert.NoError(test, err)
+	request.Header.Set(echo.HeaderAuthorization, "Bearer "+string(accessToken))
+
+	middleware := token.JWTRequiredRoleMiddleware(&privateKey.PublicKey, "administrator")
+	handler := middleware(echo.HandlerFunc(func(context echo.Context) error {
+		return context.NoContent(http.StatusOK)
+	}))
+	handler(context)
+
+	assert.Equal(test, recorder.Result().StatusCode, http.StatusForbidden)
+}
+
+func TestJWTRequiredRoleMiddlewareWithRequiredAdministratorRole(test *testing.T) {
+	server := echo.New()
+	request := httptest.NewRequest(echo.GET, "/", nil)
+	recorder := httptest.NewRecorder()
+	context := server.NewContext(request, recorder)
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 512)
+	assert.NoError(test, err)
+
+	account := entity.Account{
+		ID:    uuid.NewV4(),
+		Email: "tech+testing@mojlighetsministerietest.se",
+		Roles: []string{"user", "administrator"},
+	}
+	accessToken, err := token.Generate(privateKey, account)
+	assert.NoError(test, err)
+	request.Header.Set(echo.HeaderAuthorization, "Bearer "+string(accessToken))
+
+	middleware := token.JWTRequiredRoleMiddleware(&privateKey.PublicKey, "administrator")
+	handler := middleware(echo.HandlerFunc(func(context echo.Context) error {
+		return context.NoContent(http.StatusOK)
+	}))
+	handler(context)
+
+	assert.Equal(test, recorder.Result().StatusCode, http.StatusOK)
 }
