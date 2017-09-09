266dc977998a2a5c0f2ff78fb30a0f48c71f8586
Fredrik Söderström <fredrik.soderstrom@cambio.se>
2017-09-09 21:06:43 +0200

    Fix test

--- token/middleware_test.go
+++ token/middleware_test.go
@@ -7,6 +7,9 @@ import (
 	"testing"
 
 	"github.com/labstack/echo"
+	"github.com/mojlighetsministeriet/identity-provider/entity"
+	"github.com/mojlighetsministeriet/identity-provider/token"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -16,9 +19,19 @@ func TestGetTokenFromContext(test *testing.T) {
 	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
 	assert.NoError(test, err)
 
-	request.Header.Set(echo.HeaderAuthorization)
+	account := entity.Account{
+		ID:    uuid.NewV4(),
+		Email: "tech+testing@mojlighetsministerietest.se",
+		Roles: []string{"user"},
+	}
+	accessToken, err := token.Generate(privateKey, account)
+	assert.NoError(test, err)
+	request.Header.Set(echo.HeaderAuthorization, "Bearer "+string(accessToken))
+
+	server := echo.New()
 	recorder := httptest.NewRecorder()
 	context := server.NewContext(request, recorder)
 
-	GetTokenFromContext()
+	extractedToken := token.GetTokenFromContext(context)
+	assert.Equal(test, accessToken, extractedToken)
 }
